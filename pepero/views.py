ac1b59213908e2b5f2fe346d0db7f4ef44ff824c
dhkang <alceste951005@naver.com>
2023-11-10 02:52:53 +0900

    feat : 빼빼로 2단계 정보 세션 저장 기능

--- pepero/views.py
+++ pepero/views.py
@@ -17,7 +17,7 @@ def pepero_make_start_view(request):
         return render(request, 'peperos/loading-start.html')
     return render(request, 'peperos/loading-start.html')
 
-# 빼빼로 1다녜 만들기
+# 빼빼로 1단계 만들기
 def pepero_make_choco_view(request):
     if request.method == 'GET':
         print("빼빼로 1단계 시작")
@@ -27,25 +27,36 @@ def pepero_make_choco_view(request):
             post_data = json.loads(request.body.decode('utf-8'))
             selected_choco = post_data.get("selected_choco")
             if selected_choco:
-                print(selected_choco)
+                print("사용자 선택 초코 값 : "+selected_choco)
                 request.session['selected_choco'] = selected_choco
                 return JsonResponse({'message': 'Success'})
         except json.JSONDecodeError as e:
             return JsonResponse({'message': 'Error', 'error': str(e)})
     return render(request, 'peperos/pepero_make1.html')
 
-
+# 빼빼로 2단계
 def pepero_make_sauce_view(request):
     if request.method == 'GET':
         print("1단계에서 선택한 값 체크 :" + request.session['selected_choco'])
         print("빼빼로 2단계 시작")
         return render(request, 'peperos/pepero_make2.html')
     elif request.method == 'POST':
+        try:
+            post_data = json.loads(request.body.decode('utf-8'))
+            selected_sauce = post_data.get('selected_sauce')
+            if selected_sauce:
+                print("사용자 선택 소스 값 : "+selected_sauce)
+                request.session['selected_sauce'] = selected_sauce
+                return JsonResponse({'message': 'Success'})
+        except json.JSONDecodeError as e:
+            return JsonResponse({'message': 'Error', 'error': str(e)})
         return render(request, 'peperos/pepero_make3.html')
     
-    
+# 빼빼로 3단계
 def pepero_make_deco_view(request):
     if request.method == 'GET':
+        print("1단계에서 선택한 값 체크 :" + request.session['selected_choco'])
+        print("2단계에서 선택한 값 체크 :" + request.session['selected_sauce'])
         print("빼빼로 3단계 시작")
         return render(request, 'peperos/pepero_make3.html')
     elif request.method == 'POST':
@@ -55,6 +66,8 @@ def pepero_make_deco_view(request):
     
 def pepero_make_letter_view(request):
     if request.method == 'GET':
+        print("1단계에서 선택한 값 체크 :" + request.session['selected_choco'])
+        print("2단계에서 선택한 값 체크 :" + request.session['selected_sauce'])
         print("빼빼로 편지 쓰기 시작")
         return render(request, 'peperos/pepero_letter.html')
     elif request.method == 'POST':
