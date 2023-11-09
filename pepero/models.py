25f2d77c2509f9e87ff37274e04a094c18de049a
dhkang <alceste951005@naver.com>
2023-11-09 20:11:30 +0900

    feat : Add pepero model

--- pepero/models.py
+++ pepero/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pepero(models.Model):
+    choco = models.TextField(verbose_name='초코코팅')
+    syrop = models.TextField(verbose_name='시럽')
+    deco = models.TextField(verbose_name='지불방식')
+    content = models.TextField(verbose_name='편지 내용') 
+    
